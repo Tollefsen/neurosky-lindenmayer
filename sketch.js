a9413f0b306d8cf3c32fab6436265b8afb1fe936
Tollefsen <havard.tollefsen@gmail.com>
2019-09-19 20:27:51 +0200

    Loads of stuff
    Like faster drawing, saving, reset, etc.

--- sketch.js
+++ sketch.js
@@ -8,10 +8,13 @@ let number_of_gens;
 let extension, extension_chaos;
 let angle, angle_chaos;
 
+var email;
 var r_input;
 var g_slider;
 var e_slider, ec_slider;
 var a_slider, ac_slider;
+
+let currentIndex = 0;
 
 let r1 = 'FF[+F][--FF][-F+F]';
 let r2 = 'F[++F[-F]]F[-FF[F]]';
@@ -58,9 +61,25 @@ function generate() {
   }
 }
 
+function resetAll() {
+  resetMatrix();
+  clear();
+  currentIndex = 0;
+  for (let p = 0; p < 10; p++) {
+    pop();  
+  }
+  loop();
+}
+
+function saveDrawing() {
+  save(email.value() + '.png');
+}
+
 function setup_controllers() {
   var generate_button = createButton('generate').parent('controller');
+  var save_button = createButton('save').parent('controller');
   
+  var mail_container = createDiv('email').parent('controller');
   var r_container = createDiv('&rho; ').parent('controller');
   var g_container = createDiv('&gamma; (1-6)').parent('controller');
   var e_container = createDiv('&eta; (100-500)').parent('controller');
@@ -68,6 +87,7 @@ function setup_controllers() {
   var a_container = createDiv('&phi; (5-20)').parent('controller');
   var ac_container = createDiv('&Delta;<sub>&phi;</sub> (0-1)').parent('controller');
 
+  email = createInput('youremail', 'text').parent(mail_container);
   r_input = createInput(r1, 'text').parent(r_container);
   g_slider = createInput(5, 'number').parent(g_container);
   e_slider = createInput(200, 'number').parent(e_container);
@@ -75,7 +95,8 @@ function setup_controllers() {
   a_slider = createInput(10, 'number').parent(a_container);
   ac_slider = createInput(0.5, 'number').parent(ac_container);
 
-  generate_button.mousePressed(generate);
+  generate_button.mousePressed(resetAll);
+  save_button.mousePressed(saveDrawing);
 
   set_parametres();
 }
@@ -83,7 +104,7 @@ function setup_controllers() {
 function setup() {
   var canvas = createCanvas(700, 700);
   canvas.parent('sketch');
-  frameRate(300);
+  frameRate(60);
   setup_controllers();
   generate();
   background(216, 226, 232);
@@ -91,45 +112,52 @@ function setup() {
   resetMatrix();
 }
 
+function updateValues() {
+  if(neurosky.connectedNeurosky) {
+    const newASlider = neurosky.meditation / 100 * 15;
 
-let currentIndex = 0;
+
+    a_slider.value(newASlider + 5);
+    set_parametres();
+  }
+  
+}
+
+
+
 function draw() {
+  updateValues();
   var current_extension = extension * pow(0.5, gen);
   translate(width / 2, height);
 
   stroke(0, 40);
 
-  if(currentIndex < 20) {
-    
-    let x = sentence.charAt(currentIndex);
-    let ext = current_extension * (1 + random(-extension_chaos, extension_chaos));
-    
-    
-    let ang = angle * (1 + random(-angle_chaos, angle_chaos));
-    if (x == 'F') {
+  for(var i = 0; i < 100; i++) {
+    if(currentIndex < sentence.length) {
       pop();
-      line(0, 0, 0, -ext);
-      translate(0, -ext);
+      
+      let x = sentence.charAt(currentIndex);
+      let ext = current_extension * (1 + random(-extension_chaos, extension_chaos));
+      
+      
+      let ang = angle * (1 + random(-angle_chaos, angle_chaos));
+      if (x == 'F') {
+        line(0, 0, 0, -ext);
+        translate(0, -ext);
+      } else if (x == '+') {
+        rotate(-ang);
+      } else if (x == '-') {
+        rotate(ang);
+      } else if (x == '[') {
+        push();
+      } else if (x == ']') {
+        pop();
+      }
       push();
-    } else if (x == '+') {
-      pop();
-      rotate(-ang);
-      push();
-    } else if (x == '-') {
-      pop();
-      rotate(ang);
-      push();
-    } else if (x == '[') {
-      pop();
-      push();
-      push();
-    } else if (x == ']') {
-      pop();
-      pop();
-      push();
+    } else {
+      noLoop();
     }
-  } else {
-    noLoop();
+    currentIndex++;
   }
-  currentIndex++;
+  
 }
